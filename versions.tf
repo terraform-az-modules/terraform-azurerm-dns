67833ff005eeb56b8235d0baa7b19674f133760f
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 15:55:52 -0400

    chore: polish module with basic example, changelog, and version fixes (#12)
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -1,20 +1,12 @@
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.86.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0" # Your version constraint here
+      version = "# Terraform version"
     }
   }
-
   provider_meta "azurerm" {
     module_name = "terraform-az-modules/terraform-azurerm-dns"
   }
